8d3ca78a73d710ebdc7007473c866a608ab02295
rbmr <robertmertens10@gmail.com>
2025-12-23 00:05:19 +0100

    fix quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nerdbowl.github.io/nerd-bowl-site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
